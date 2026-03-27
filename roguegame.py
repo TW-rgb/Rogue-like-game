5a162a2375013fce8f42995fcbb55101e3b7de09
TW-rgb <twiley@gocfs.net>
2026-03-27 11:25:21 -0400

    Update roguegame.py

--- roguegame.py
+++ roguegame.py
@@ -4,3 +4,31 @@ import pygames
 import math
 import socket
 import threading
+
+
+class Player:
+    def __init__(self ,Hp ,Mp ,Strength ,Speed ,Defense ,Intellgence ,inventory ,level ,user_class ,x ,y ,bullet):
+        self.Hp
+        #hp points, depends on user_class
+        self.Mp
+        #attacks - from mp for magecraft
+        self.Strength
+        #base damage, depends on user_class
+        self.Speed
+        #base speed, depends on user_class
+        self.Defense
+        #damage reduction
+        self.Intellgence
+        #changes magic damage, base increase depends on user_class
+        self.inventory
+        #pulls items from dungeon, effects(boomerang gun, ice effect, idk)
+        self.level
+        #scales exponetly with higher levels, randomized xp requirement but still exponetial
+        self.user_class
+        #picks at beggining of game
+        self.x
+        #location x
+        self.y
+        #location y
+        self.bullet
+        #sub classes - bullet speed, bullet size, bullet path(boomerang type shish), bullet effects
