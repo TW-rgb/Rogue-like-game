4642c8d3378f7475cc67126b0e19722c57f213b9
bheng-python <bheng@gocfs.net>
2026-04-28 18:57:51 -0400

    Update roguegame.py

--- roguegame.py
+++ roguegame.py
@@ -1,11 +1,13 @@
+#various imports
 import struct
 import time
 import pygame
 import math
 import socket
+#handling multiple things at once.
 import threading
 
-
+#player class
 class Player:
     def __init__(self ,Hp ,Mp ,Strength ,Speed ,Defense ,Intellgence ,inventory ,level ,user_class ,x ,y ,bullet):
         self.Hp
@@ -32,26 +34,30 @@ class Player:
         #location y
         self.bullet
         #sub classes - bullet speed, bullet size, bullet path(boomerang type shish), bullet effects
-<<<<<<< HEAD
-run = 0
-=======
+
 
 
 # starting variables
->>>>>>> cffc1a8a16ab84c465da0bdf74f3c281309794dc
 x = 32
 y = 32
+#box x axis and y axis
 boxy = 0
 boxx = 0
-x_floor = 32
-y_floor = 32
+#for floors
+x_floor2 = 0
+y_floor2 = 0
 floor_running = True
+#idk
 counter = 1
+#wall Score
+wall_score = 10
 
 pygame.init()
+#images
 screen = pygame.display.set_mode((640,640))
 floor = pygame.image.load('Grassfloor1.png')
 floor2 = pygame.image.load('Grassfloor2.png')
+#basic start stuff
 running = True
 clock = pygame.time.Clock()
 
@@ -72,6 +78,7 @@ boxes = screen.width/50
 
 #the actual game
 while running:
+
 
     #player
     player = pygame.Rect(x,y,32,32)
@@ -114,72 +121,75 @@ while running:
             print(boxy)
             print(boxx)
         counter = 0
-<<<<<<< HEAD
     for platform in platforms:
         pygame.draw.rect(screen, (255,0,0), platform, 10)
-=======
     #wall code end
     # floor code start
-    while x_floor < 608:
-        screen.blit(floor, (x_floor, y_floor))
-        x_floor += 32
-        y_floor = 32
-    while y_floor < 608:
-        screen.blit(floor2, (x_floor, y_floor))
-        y_floor += 32
-        x_floor = 32
+    #floor code for x axis
+    if x_floor2 < 641:
+        screen.blit(floor2, (x_floor2, y_floor2))
+        x_floor2 += 32
+        print("floor2")
+    #floor code to move down 32
+    if x_floor2 > 608:
+        screen.blit(floor, (x_floor2, y_floor2))
+        x_floor2 = 0
+        y_floor2 += 32
+    #resets floor code to beggiing
+    if y_floor2 > 640 and x_floor2 > 640:
+        y_floor2 = 32
+        x_floor2 = 32
+
     # floor code end
     # begging of the drawing process
     blitall(platforms)
     blitall(floors)
->>>>>>> cffc1a8a16ab84c465da0bdf74f3c281309794dc
     pygame.draw.rect(screen, (0,255,0), player, 2)
     # ending
-
+    #gets key presses single cuz 32 bit system
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
+            #debug mode
             if event.key == pygame.K_d:
                 if debug_mode == True:
                     debug_mode = False
                 elif debug_mode == False:
                     debug_mode = True
+            #movement for y axis
             if event.key == pygame.K_UP:
                 y -= 32
             if event.key == pygame.K_DOWN:
                 y += 32
+            #movement for x axis
             if event.key == pygame.K_LEFT:
                 x -= 32
             if event.key == pygame.K_RIGHT:
                 x += 32
+            #quit button
             if event.type == pygame.QUIT:
                 running = False
+            #wall code up
             if x < 32:
                 x = 32
                 print("you hit a wall")
+                wall_score -= 1
+            #wall code down
             if x > 608:
                 x = 608
                 print("you hit a wall")
+                wall_score -= 1
+            #wall code left side
             if y < 32:
                 y = 32
                 print("you hit a wall")
+                wall_score -= 1
+            #wall code right side
             if y > 608:
                 y = 608
                 print("you hit a wall")
-    if run > 0:
-        while floor_running:
-            while x_floor < 640:
-                print("x_floor")
-                screen.blit(floor, (x_floor, y_floor))
-                x_floor = x_floor + 32
-                y_floor = 32
-            while y_floor < 640:
-                print("y_floor")
-                screen.blit(floor2, (x_floor, y_floor))
-                y_floor = y_floor + 32
-                x_floor = 32
-            if x_floor == 608 and y_floor == 608:
-                floor_running = False
+                wall_score -= 1
+    #prints wall score/score
+    print(wall_Score)
     clock.tick(60)
     pygame.display.flip()
-    run += 1
 pygame.quit()
